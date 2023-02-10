81a977ec8ed05db707a6534c3f692729d4a93567
Emilio Bazan <johndoe@example.com>
2023-02-10 11:24:49 -0600

    bola blanca y mas ancho de pantalla

---
+++ script.py
@@ -0,0 +1,37 @@
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Set up window
+window_size = (1000, 500)
+window = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Star Visualization")
+
+# Set up clock
+clock = pygame.time.Clock()
+
+# Define star parameters
+star_color = (255, 255, 255)
+star_position = (250, 250)
+star_radius = 50
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Clear screen
+    window.fill((0, 0, 0))
+
+    # Draw star
+    pygame.draw.circle(window, star_color, star_position, star_radius)
+
+    # Update display
+    pygame.display.update()
+    clock.tick(60)
+
+# Quit game
+pygame.quit()
